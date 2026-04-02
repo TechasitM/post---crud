2650ddfb0a75894aaf053bc6a541ce3fdb2b3769
TechasitM <148959066+TechasitM@users.noreply.github.com>
2026-04-02 21:56:58 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,5 @@ Route::post('/store', [PostController::class, 'store']) ->name('store');
 Route::get('/show{post}', [PostController::class, 'show']) ->name('show');
 Route::get('/edit{post}', [PostController::class, 'edit']) ->name('edit');
 Route::put('/update{post}', [PostController::class, 'update']) ->name('update');
-Route::delete('/delete{post}', [PostController::class, 'delete']) ->name('delete');
+Route::delete('/delete{post}', [PostController::class, 'destroy']) ->name('delete');
 
